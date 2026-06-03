5f17490e47902b9a76b7720c85680f4e8c41e1d4
SujayAmberkar <sujayamberkar@gmail.com>
2026-06-03 11:15:07 +0200

    showing random color on the screen

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
+#include<iostream>
 #include <GLFW/glfw3.h>
+#include "vector"
+#include <random>
+#include <chrono>
 
 int main(void)
 {
     GLFWwindow* window;
+    int renderTime = 0;
+    int width = 640;
+    int height = 480;
+    std::vector<float> pixelValues;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "My RayTracer", NULL, NULL);
+    window = glfwCreateWindow(width, height, "My RayTracer", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -19,11 +27,28 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    std::mt19937 rng(42);
+    std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+
+
+    // create a color array
+    for (int i = 0; i < width * height * 4; i++) {
+        pixelValues.push_back(dist(rng));
+    }
+
+    
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        glDrawPixels(width, height, GL_RGBA, GL_FLOAT, pixelValues.data());
+        auto end = std::chrono::high_resolution_clock::now();
+        renderTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -31,6 +56,7 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+    std::cout << renderTime <<"ms";
 
     glfwTerminate();
     return 0;
