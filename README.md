5cf4f63b7af56d50d1c929df803399717688a27a
SujayAmberkar <sujayamberkar@gmail.com>
2026-06-13 23:54:25 +0200

    added Readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# MyRayTracer
+# Ray Tracer
+
+## Overview
+
+A personal project to learn Ray Tracing while building a Ray Tracer. 
+Started as semester project for Advanced Computer graphics, I plan to
+keep working on this project to learn and implement even more advanced
+techniques. Currently I'm following the [_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
+book to get a skeleton ray tracer, while also changing(or trying to) some parts
+of the implementation. 
+
+## Current Render
+![Rendered Image]("render.png")
+
+## Implemented Features
+
+- Whitted style Ray Tracing.
+- Render final image in a GLFW window.
+- ImGUI UI that shows render time, editable samples per pixel & ray depth, and re-render button.
+- Implemented Materials:
+    - Diffused Surface
+    - Metal(smooth & matte)
+    - Dielectric (Reflection + Refraction)
+
+## Build and Run
+```bash
+
+ git clone https://github.com/SujayAmberkar/MyRayTracer.git
+ cd MyRayTracer
+```
+Open MyRayTracer.sln in Visual Studio(I used VS 2022).
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
-
+#define _CRT_SECURE_NO_WARNINGS
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #include <GLFW/glfw3.h>
 #include "vector"
 #include "../header/common.h"
@@ -10,6 +12,9 @@
 #include "../header/hittable_list.h"
 #include "../header/material.h"
 #include "../header/sphere.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 
 #define IMAGE_WIDTH_CONST 800
@@ -38,8 +43,8 @@ int main(void)
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = IMAGE_WIDTH_CONST;
-    cam.samples_per_pixel = 30;
-    cam.max_depth = 10;
+    cam.samples_per_pixel = 50;
+    cam.max_depth = 20;
 
     // to Calculate the render time
     auto start = std::chrono::high_resolution_clock::now(); // start the clock
@@ -55,6 +60,13 @@ int main(void)
     /* Create a windowed mode window and its OpenGL context */
     int image_width = IMAGE_WIDTH_CONST;
     int image_height = int(IMAGE_WIDTH_CONST / cam.aspect_ratio);
+    
+    std::vector<unsigned char> pngPixels(pixelValues.size());
+    for (int i = 0; i < pixelValues.size(); i++) {
+        pngPixels[i] = (unsigned char)(pixelValues[i] * 255.99f);
+    }
+    stbi_flip_vertically_on_write(1);
+    stbi_write_png("render.png", image_width, image_height, 3, pngPixels.data(), image_width * 3);
 
     window = glfwCreateWindow(image_width, image_height, "My RayTracer", NULL, NULL);
     if (!window)
@@ -67,6 +79,13 @@ int main(void)
     glfwMakeContextCurrent(window);
     std::mt19937 rng(42);
     std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+
+    // create IMGUI window
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
     
     // main game looop
     while (!glfwWindowShouldClose(window))
@@ -74,8 +93,29 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        
+
         // draw pixels on the screen from the pixel values array
         glDrawPixels(image_width, image_height, GL_RGB, GL_FLOAT, pixelValues.data());
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+
+        ImGui::Begin("Stats");
+        ImGui::Text("Render Time: %dms", renderTime);
+        ImGui::SliderInt("Samples", &cam.samples_per_pixel, 1, 100);
+        ImGui::SliderInt("Max Depth", &cam.max_depth, 1, 50);
+        if (ImGui::Button("Render")) {
+            pixelValues.clear();
+            cam.render(world, pixelValues);
+        }
+        ImGui::End();
+
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -84,7 +124,9 @@ int main(void)
         glfwPollEvents();
     }
     std::cout << renderTime <<"ms"; // render time will be printed after the window is closed for now. !NEED TO SHOW ON THE SCREEN!
-
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }
